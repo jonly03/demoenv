dde630137bfa843a2ae803db97466afe6ef37c71
Nelly Sugu <jonly03@gmail.com>
2020-07-20 22:20:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+// GET /movies route
+// expects an s query parameter
+// Go to OMDB API and search for s
+app.get("/movies", (req, res) => {
+  // fetch http://www.omdbapi.com/?apikey=&s=req.query.s
+  const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_APIKEY}&s=${req.query.s}`;
+  axios.get(url).then((movies) => {
+    return res.json(movies.data);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
